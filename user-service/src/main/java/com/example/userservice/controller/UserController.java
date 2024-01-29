4e826a05ef7aeebb387badac567da796a59e3fc0
DDonghyeo <gsu9045000@gmail.com>
2024-01-29 19:33:34 +0900

    feat: Config Server

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,8 +1,8 @@
 package com.example.userservice.controller;
 
-import com.netflix.discovery.converters.Auto;
-import lombok.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,17 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
-    @Autowired
-    Environment environment;
+    @Value("${jwt.secret}")
+    private String secret;
 
     @GetMapping("/test")
+
     public String test() {
 
-        String secret = environment.getProperty("jwt.secret");
-
-
-        return "User Service Test: " +
-                "token secret : " +  secret;
+        return "token secret : " + secret ;
     }
 
 }
